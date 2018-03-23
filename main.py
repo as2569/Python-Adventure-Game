b1e0d1f6be698830191ae6e3fb6c370101fe7a79
Adam Spryszynski <as2569@njit.edu>
2018-03-23 14:30:36 -0400

    python adventure game initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+import sys
+
+class Planet():
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+            
+def TakeInput():
+    in_str = input("Enter Command \n")
+    in_str.lstrip()
+    if in_str == "exit":
+        print("Goodbye")
+        sys.exit()
+    else:
+        print("Invalid command")
+
+def Setup():
+    grid = [x][y].:Cell
+    
+while True:
+    TakeInput()
+
+    
