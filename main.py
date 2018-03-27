800738f2fb3df39b60313ab4f09654c6a62599fb
Adam Spryszynski <as2569@njit.edu>
2018-03-26 23:02:02 -0400

    wrapped input into input manager class, added movement

--- main.py
+++ main.py
@@ -5,21 +5,64 @@ def GetPosition():
     print("Current location {0} {1}".format(player.position.x, player.position.y))
     return player.position
 
-def TakeInput():
-    in_str = input("Enter Command \n")
-    in_str.lstrip()
-    if in_str == "exit":
-        print("Goodbye")
-        sys.exit()
-    else:
-        print("Invalid command")
+
+class InputManager():
+    def TakeInput(self):
+        inString = input("Enter Command \n")
+        inString.lstrip()
+        inString.lower()
+        inList = inString.split()
+        if len(inList) > 2:
+            print("Too many inputs")
+            return
+        if len(inList) == 0:
+            print("No input")
+            return
+        
+        if inList[0] == "exit":
+            print("Goodbye")
+            sys.exit()
+        elif inList[0] == "move":
+            if(len(inList) < 2):
+                print("Move command requires a direction")
+                return
+            self.Move(inList[1])
+        else:
+            print("Invalid command")
+
+    def Move(self, direction):
+        if direction == "north":
+            if(player.position.y - 1 >= 0):
+                print("Moving north!")
+                player.MoveNorth()
+                return
+        elif direction == "south":
+            if(player.position.y + 1 < planetMap.height):
+                print("Moving south!")
+                player.MoveSouth()
+                return
+        
+        print("Nothing but void that way!")
+        return
 
 class Player():
     position = None
 
     def NewPlayer(self):
         self.position = planetMap.GetRandomPlanet()
-                
+
+    def MoveNorth(self):
+        self.position = planetMap.GetPlanetAt(self.position.x, self.position.y - 1)
+
+    def MoveSouth(self):
+        self.position = planetMap.GetPlanetAt(self.position.x, self.position.y + 1)
+
+    def MoveEast(self):
+        self.position = planetMap.GetPlanetAt(self.position.x + 1, self.position.y)
+
+    def MoveWest(self):
+        self.position = planetMap.GetPlanetAt(self.position.x - 1, self.position.y)
+        
 class Planet():
     def __init__(self, x, y):
         self.x = x
@@ -44,7 +87,8 @@ class Map():
         x = random.randrange(0, self.width - 1)
         y = random.randrange(0, self.height - 1)
         return self.matrix[(x, y)]
-        
+
+manager = InputManager()
 planetMap = Map(10, 10)
 planetMap.NewMap()
 
@@ -53,6 +97,6 @@ player.NewPlayer()
 
 while True:
     GetPosition()
-    TakeInput()
+    manager.TakeInput()
 
     
